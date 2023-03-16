8dfe6c0b33d747fdc1bdc152e1f32cab0ba496f2
Tushar-ShahidurRahman <ptushar004@gmail.com>
2023-03-16 17:15:45 +0600

    handled 401 status code

--- lib/data/network_utils.dart
+++ lib/data/network_utils.dart
@@ -1,51 +1,76 @@
 import 'dart:convert';
+import 'dart:developer';
 
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:task_manager_rest_api/data/auth_utils.dart';
+import 'package:task_manager_rest_api/main.dart';
+import 'package:task_manager_rest_api/ui/screens/login_screen.dart';
 
 class NetworkUtils {
-//getMethond
+//getMethod
 
   static Future<dynamic> getMethod(String url,
       {VoidCallback? onUnAuthorized}) async {
     try {
       final http.Response response = await http.get(Uri.parse(url));
-      if (response.statusCode == 200) {
-        return jsonDecode(response.body);
-      } else if (response.statusCode == 401) {
-        print('Unauthorized');
-      } else {
-        print('Something went wrong');
-      }
-    } catch (e) {
-      print(e);
-    }
-  }
-
-  // postMethod
-  static Future<dynamic> postMethod(String url,
-      {Map<String, String>? body, VoidCallback? onUnAuthorized, String? token,}) async {
-    try {
-      final http.Response response = await http.post(
-        Uri.parse(url),
-        headers: {
-          "Content-Type": "application/json",
-          'token': token?? '',
-        },
-        body: jsonEncode(body),
-      );
-      print(response.body);
+      log(response.body);
       if (response.statusCode == 200) {
         return jsonDecode(response.body);
       } else if (response.statusCode == 401) {
         if (onUnAuthorized != null) {
           onUnAuthorized();
+        } else {
+          moveToLogin();
         }
       } else {
-        print('Something went wrong');
+        log('Something went wrong');
       }
     } catch (e) {
-      print(e);
+      log('error $e');
     }
+  }
+
+  // postMethod
+  static Future<dynamic> postMethod(
+    String url, {
+    Map<String, String>? body,
+    VoidCallback? onUnAuthorized,
+    String? token,
+  }) async {
+    try {
+      final http.Response response = await http.post(
+        Uri.parse(url),
+        headers: {
+          "Content-Type": "application/json",
+          'token': token ?? '',
+        },
+        body: jsonEncode(body),
+      );
+      log(response.body);
+      if (response.statusCode == 200) {
+        return jsonDecode(response.body);
+      } else if (response.statusCode == 401) {
+        if (onUnAuthorized != null) {
+          onUnAuthorized();
+        } else {
+          moveToLogin();
+        }
+      } else {
+        log('Something went wrong');
+      }
+    } catch (e) {
+      log('Error $e');
+    }
+  }
+
+  static void moveToLogin() async {
+    await AuthUtils.clearAuthData();
+    Navigator.pushAndRemoveUntil(
+        TaskManagerApp.navigatorGlobalKey.currentContext!,
+        MaterialPageRoute(
+          builder: (context) => const LoginScreen(),
+        ),
+        (route) => false);
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -4,17 +4,24 @@ import 'package:task_manager_rest_api/ui/screens/login_screen.dart';
 import 'ui/screens/splash_screen.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const TaskManagerApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class TaskManagerApp extends StatefulWidget {
+  const TaskManagerApp({Key? key}) : super(key: key);
+  static GlobalKey<NavigatorState> navigatorGlobalKey = GlobalKey();
 
+  @override
+  State<TaskManagerApp> createState() => _TaskManagerAppState();
+}
+
+class _TaskManagerAppState extends State<TaskManagerApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: SplashScreen(),
+      navigatorKey: TaskManagerApp.navigatorGlobalKey,
+      home: const SplashScreen(),
     );
   }
 }
